51369a72e2d88c14c3959b1d7501fc5a64c05e0d
ndigor <108065561+ndigor@users.noreply.github.com>
2022-10-06 11:17:34 +0300

    Задача №3
    Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.

---
+++ Program.py
@@ -0,0 +1,13 @@
+﻿#Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+numbers = [1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7]
+
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+print(get_unique_numbers(numbers))
